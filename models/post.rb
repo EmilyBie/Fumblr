a91dfeaf37e654ffe4f891eb5aa6a237d0b6f80e
Emily Bie <bieyuyu@gmail.com>
2016-04-22 13:41:49 +1000

    fog related

--- models/post.rb
+++ models/post.rb
@@ -25,7 +25,7 @@ class MyUploader < CarrierWave::Uploader::Base
   #   process resize_to_fill: [200,200]
   # end
   
-  storage :file
+  storage :fog
 
   def extension_whitelist
     %w(jpg jpeg gif png)
